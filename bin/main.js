cc8b6716b17abdd05843781f83ae016d528de952
maartenvandenbrande <maarten.vdbrande@gmail.com>
2022-08-23 09:48:56 +0200

    before .gitignore is working

--- bin/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger_1 = require("./utils/logger");
-let logger = logger_1.Logger.getInstance(logger_1.LogType.debug);
-logger.error("error", "root");
