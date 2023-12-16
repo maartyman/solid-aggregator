b013a310dc6f9cf3df8a964fc5aef596f358c720
maartenvandenbrande <maarten.vdbrande@gmail.com>
2023-12-16 19:10:42 +0100

    remove old stuff

--- src/utils/loggerSettings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerSettings = void 0;
-exports.loggerSettings = {
-    minLevel: "warn",
-    instanceName: "Solid Aggregator Server",
-    hostname: "localhost"
-};
--- src/utils/loggerSettings.ts
+++
@@ -1,7 +0,0 @@
-import {ISettingsParam} from "tslog";
-
-export let loggerSettings: ISettingsParam = {
-  minLevel: "warn",
-  instanceName: "Solid Aggregator Server",
-  hostname: "localhost"
-};
