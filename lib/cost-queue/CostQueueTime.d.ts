f0e909c63e77f7d055e488c994382718cada0787
maartenvandenbrande <maarten.vdbrande@gmail.com>
2023-12-18 12:20:18 +0100

    remove all `.d.ts`, `.js`, and `.js.map` files

--- lib/cost-queue/CostQueueTime.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CostQueue, CostQueueFactory } from './CostQueue';
-export declare class CostQueueTimeFactory implements CostQueueFactory {
-    create(): CostQueue;
-}
--- lib/endpoint/endpoint.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Endpoint {
-    private readonly httpServer;
-    constructor();
-    run(): void;
-}
