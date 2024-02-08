9b4dd64756ab7f913062f5a9f96e6d44bf7163e9
maartenvandenbrande <maarten.vdbrande@gmail.com>
2024-02-08 15:38:59 +0100

    Update AppRunner.ts

--- lib/init/AppRunner.ts
+++ lib/init/AppRunner.ts
@@ -7,7 +7,7 @@ export class AppRunner {
     const manager = await ComponentsManager.build({
       mainModulePath: path.join(__dirname, '../..'),
     });
-    await manager.configRegistry.register('../../config/default.json');
+    await manager.configRegistry.register('config/default.json');
     // eslint-disable-next-line unused-imports/no-unused-vars
     const myInstance: Endpoint = await manager.instantiate('urn:solid-aggregator:endpoint');
   }
